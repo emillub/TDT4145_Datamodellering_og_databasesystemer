71f546953c5f9732c475617c7e66aed9f94e6663
emillub <145599402+emillub@users.noreply.github.com>
2024-03-15 12:54:52 +0100

    Scan hovedscenen work

--- files/scan-seats.py
+++ files/scan-seats.py
@@ -44,5 +44,59 @@ def lesSolgteSeterFraFil(fil, resetOnNewArea):
                 elif(sete == '1'):
                     solgteSeter[dato][omraade].append((radIndex-radIndexOffset,seteIndex-seteIndexOffset))
         return solgteSeter
+
+def inneholderOmraderEllerDato(line):
+    return any((not c.isdigit() and c!='x') for c in line) # Sjekker om linje inneholder dato eller område
+
+def scanHovedScenen():
+    fil = 'txt/hovedscenen.txt'
+    kapasitet = 524
+    rader = 22 # teller med Galleri
+    forestillinger = {}
+    stolerIForegaendeRader = 0
+    with open(fil, 'r') as f:
+        linjer = f.readlines()
+        radIndexOffset = rader
+        for radIndex, rad in enumerate(linjer):
+            rad = rad.strip() # Fjerner tomrom fra linje
+            if inneholderOmraderEllerDato(rad):
+                radIndexOffset +=1
+                if 'Dato' in rad:
+                    dato = rad.strip('Dato ')
+                    datoDic = {dato : []}
+                    forestillinger[dato] = {
+                        'sal' : 'Hovedscenen',
+                        'solgteSeter' : {},
+                        'tommeSeter' : {}
+                    }
+                else:
+                    omraade = rad
+                    forestillinger[dato]['solgteSeter'][omraade] = []
+                    forestillinger[dato]['tommeSeter'][omraade] = []
+                    print(f"Område: {rad}")
+                continue
+
+            xer = 0
+            print(len(rad))
+            for stolIndex,stol in enumerate(rad):
+                stolnr = kapasitet-stolerIForegaendeRader-stolIndex
+                if stol == '1':
+                    forestillinger[dato]['solgteSeter'][omraade].append((radIndexOffset-radIndex, stolnr))
+                elif stol == 'x':
+                    forestillinger[dato]['tommeSeter'][omraade].append((radIndexOffset-radIndex, stolnr))
+            stolerIForegaendeRader+=len(rad)
+        f.close()
+    return forestillinger
     
-print(lesSolgteSeterFraFil('txt/hovedscenen.txt',False))
+print(scanHovedScenen())
+                
+                
+            
+
+
+
+
+            
+            
+
+# print(lesSolgteSeterFraFil('txt/hovedscenen.txt',False))
