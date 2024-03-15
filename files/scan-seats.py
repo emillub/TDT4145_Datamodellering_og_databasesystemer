4fca2c02e75cfd9e7839a25cf69628d9d734b6ee
emillub <145599402+emillub@users.noreply.github.com>
2024-03-15 11:14:36 +0100

    Reset on new area option in scan-seats() work

--- files/scan-seats.py
+++ files/scan-seats.py
@@ -7,25 +7,36 @@
     Returnerer:
     solgteSeter (Dict): Dictionary for solgte seter i en gitt oppsetning på formatet {dato: {område: [(radnr,setenr)]}}
 """
-def lesSolgteSeterFraFil(fil):
-    with open(fil,'r') as f:
 
-        radIndexOffset = -1 #Paser på at linjer som inneholder tekst ikke regnes som en stolrad
+def lesSolgteSeterFraFil(fil, resetOnNewArea):
+    with open(fil,'r') as f:
+        radIndexOffset = -1 # Passer på at linjer som inneholder tekst ikke regnes som en stolrad
         solgteSeter = {}
+        omraade = None
+        seteIndexOffset = 1
         for radIndex, linje in enumerate(f.readlines()):
-            seteIndexOffset = -1
+            if resetOnNewArea:
+                seteIndexOffset = -1
             linje = linje.strip() # Fjerner tomrom fra linje
-            linjeHarBokstaver = any((not c.isdigit() and c!='x') for c in linje)
+            linjeHarBokstaver = any((not c.isdigit() and c!='x') for c in linje) # Sjekker om linje inneholder dato eller område
             if linjeHarBokstaver:
                 radIndexOffset +=1
                 if 'Dato' in linje:
                     dato = linje.strip('Dato ')
                     solgteSeter[dato] = {}
                 else:
+                    if resetOnNewArea and omraade is not None: 
+                        radIndexOffset = radIndex
+
                     omraade = linje
                     solgteSeter[dato][omraade] = []
                     print(f"Område: {linje}")
                 continue
+            if not resetOnNewArea:
+                seteIndexOffset -= len(linje) -1
+                print(seteIndexOffset)
+                print(f'Antall seter ({len(linje)})')
+
 
             for seteIndex,sete in enumerate(linje):
                 if(sete =='x'):
@@ -34,4 +45,4 @@ def lesSolgteSeterFraFil(fil):
                     solgteSeter[dato][omraade].append((radIndex-radIndexOffset,seteIndex-seteIndexOffset))
         return solgteSeter
     
-print(lesSolgteSeterFraFil('txt/gamle-scene.txt'))
+print(lesSolgteSeterFraFil('txt/hovedscenen.txt',False))
