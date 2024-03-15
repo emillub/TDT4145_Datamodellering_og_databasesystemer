31647fbb89a81498fedd9ef306ba24fcca0005b3
emillub <145599402+emillub@users.noreply.github.com>
2024-03-15 09:34:50 +0100

    Create entities.py

---
+++ files/enteties.py
@@ -0,0 +1,13 @@
+Ansatt = {
+    'AnsattID': int,
+    'Navn': str,
+    'Email': str,
+    'AnsattStatus': str
+}
+
+ArbeidsOppgave = {
+    'OppgaveID': int,
+    'Navn': str,
+    'Beskrivelse': str,
+    'TeaterStykleID' : int
+}
--- files/scan-seats-hovedscenen.py
+++ files/scan-seats-hovedscenen.py
@@ -3,7 +3,5 @@ import sqlite3
 con = sqlite3.connect("./teater.db")
 cursor = con.cursor()
 
-
-
 con.commit()
 con.close()
